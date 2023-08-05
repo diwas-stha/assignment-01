6a6f65f4a656d54408f399e97a8bafd201d4a0b4
diwas-stha <diwas@fusemachines.com>
2023-08-05 18:24:00 +0545

    checking pre-commit hook

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 def addition(num1, num2):
-    """Perform addition of two numbers. """
+    """Perform addition of two numbers."""
     return num1 + num2
+
 
 def subtraction(num1, num2):
     """Perform subtraction of two numbers."""
     return num1 - num2
 
+
 def multiplication(num1, num2):
     """Perform multiplication of two numbers."""
     return num1 * num2
+
 
 def division(num1, num2):
     """Perform division of two numbers."""
     if num2 == 0:
         print("Division by zero is not allowed!")
     return num1 / num2
+
 
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
